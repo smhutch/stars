a2f9e3272b57037723ce3734040d058a8a87d695
Scott Hutcheson <scott@smhutch.co.uk>
2021-09-12 17:24:55 +0100

    tooling: add minimal linter

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: "eslint:recommended",
+  rules: {},
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+};
